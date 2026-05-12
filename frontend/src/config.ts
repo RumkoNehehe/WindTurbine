a1262641f57b3dae6d3aa330c9e689b0293eb9f6
Rumko <matorumanovic@gmail.com>
2026-05-12 16:20:54 +0200

    ngix introduced, docker file changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  // backendBaseUrl: "http://100.118.128.111:8000",
-  backendBaseUrl: "http://localhost:8000",
+  backendBaseUrl: "http://100.78.68.88:8000",
+  // backendBaseUrl: "http://localhost:8000",
   
 };
